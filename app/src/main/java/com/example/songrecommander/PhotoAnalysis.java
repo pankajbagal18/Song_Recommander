138b0941b61739f329b1c6bbb9c50fd4f7ec8d39
Pankaj <pankajsbagal@gmail.com>
2020-04-15 17:01:29 +0530

    removed FaceOverlay class

--- app/src/main/java/com/example/songrecommander/PhotoAnalysis.java
+++ app/src/main/java/com/example/songrecommander/PhotoAnalysis.java
@@ -15,38 +15,9 @@ import android.widget.Toast;
 
 public class PhotoAnalysis extends AppCompatActivity {
 
-    private FaceOverlay mFaceOverlayView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_analysis);
-        mFaceOverlayView = (FaceOverlay)findViewById(R.id.face_overlay);
-        String photoPath = getIntent().getStringExtra("PHOTO_PATH");
-        mFaceOverlayView.setBitmap(BitmapFactory.decodeFile(photoPath));
-
-        /*image = (ImageView) findViewById(R.id.currentPhoto);
-        String photoPath = getIntent().getStringExtra("PHOTO_PATH");
-        if(photoPath==null)
-            Toast.makeText(getApplicationContext(),"null image",Toast.LENGTH_LONG).show();
-        else {
-            Toast.makeText(getApplicationContext(),"path : "+photoPath,Toast.LENGTH_LONG).show();
-            int targetW = image.getWidth();
-            int targetH = image.getHeight();
-
-            // Get the dimensions of the bitmap
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inJustDecodeBounds = true;
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inPurgeable = true;
-            Bitmap photoBitmap = BitmapFactory.decodeFile(photoPath,bmOptions);
-            image.setImageBitmap(photoBitmap);
-        }
-        backButton = (Button)findViewById(R.id.backButton);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-            }
-        });*/
     }
 }
